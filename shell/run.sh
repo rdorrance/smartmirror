705b3c114ff13cedf02385ae8572690eefeebedc
rdorrance <rdorrance@gmail.com>
2016-05-08 21:00:06 -0700

    Auto restart kweb on crash
    Auto restart kweb on crash

--- shell/run.sh
+++ shell/run.sh
@@ -4,3 +4,11 @@
 cd /home/pi/smartmirror
 # start display controller
 # sudo python shell/display-control.py
+
+echo "Starting kweb in 5 seconds..."
+sleep 5
+while :
+    do
+        kweb -KHCUJ http://localhost/~pi
+done
+
