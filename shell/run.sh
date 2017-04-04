180f42747e582b51cba124ddbf35ac26d1048226
rdorrance <rdorrance@gmail.com>
2017-04-03 19:55:45 -0700

    for larger monitor
    updates

--- shell/run.sh
+++ shell/run.sh
@@ -9,7 +9,6 @@ echo "Starting kweb in 5 seconds..."
 sleep 5
 while :
     do
-        #kweb -KHCUJ http://localhost/~pi
-		chromium --kiosk --disable-sync --disable-translate --incognito http://localhost/~pi
+        kweb -KHCUJ http://localhost/~pi
 done
 
